2acbf665f3cecbc229098031c05d95fe41813abb
Ariel Navas <arielnavas05@gmail.com>
2024-12-22 18:13:29 -0500

    test deploy

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
       <header className="bg-teal-400 py-5">
-        <h1 className="text-center text-4xl font-black">Calculadora de propinas DAS</h1>
+        <h1 className="text-center text-4xl font-black">Calculadora de propinas </h1>
       </header>
 
       <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
